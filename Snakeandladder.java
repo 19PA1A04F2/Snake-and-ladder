7357f6ab192647b7230207a97af245395246ece9
19PA1A04F2 <61041868+19PA1A04F2@users.noreply.github.com>
2021-12-13 22:00:47 +0530

    Create Snakeandladder.java

---
+++ Snakeandladder.java
@@ -0,0 +1,10 @@
+package Snakeandladdergame;
+class Snakeandladder {
+    int startPoint;
+    int endPoint;
+
+    public Snakeandladder(int startPoint, int endPoint) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+    }
+}
