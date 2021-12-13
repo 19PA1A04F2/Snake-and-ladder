8dc155489ed6a1081750af00e99f132a9c187bda
19PA1A04F2 <61041868+19PA1A04F2@users.noreply.github.com>
2021-12-13 21:59:00 +0530

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,16 @@
+package Snakeandladdergame;
+class Player {
+    private String playerName;
+    private int id;
+
+    Player(String playerName, int id) {
+        this.playerName = playerName;
+        this.id = id;
+    }
+    public String getPlayerName(){
+        return playerName;
+    }
+    public int getid(){
+        return id;
+    }
+}
